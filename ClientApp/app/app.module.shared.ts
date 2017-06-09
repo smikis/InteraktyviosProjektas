068e4597a083442eb1611fa895d6ec0730053e01
tomval <tomovaliuno@gmail.com>
2017-06-09 18:29:40 +0300

    Add project files.

---
+++ ClientApp/app/app.module.shared.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { HelloWorldComponent } from './components/helloworld/helloworld.component';
+import { WeatherComponent } from './components/weather/weather.component';
+
+export const sharedConfig: NgModule = {
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent,
+        HelloWorldComponent,
+        WeatherComponent
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'hello', component: HelloWorldComponent },
+            { path: 'weather', component: WeatherComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+};

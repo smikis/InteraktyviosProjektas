9f76e4cdcc61505890cb59392cb5a054722b86dd
tomval <tom@tom.tom>
2017-06-14 11:41:07 +0300

    Product list

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Ng2UploaderModule } from 'ng2-uploader';
 import { CustomFormsModule } from 'ng2-validation';
 import { NguiPopupModule } from '@ngui/popup';
+import { DataTableModule } from "angular2-datatable";
 
 import { AuthService } from './_services/auth.service';
 
@@ -15,6 +16,7 @@ import { LoginComponent } from './components/login/login.component';
 import { NewProductComponent } from './components/newproduct/newproduct.component';
 import { ProductDetailsComponent } from './components/details/productdetails.component';
 import { ProductListItemComponent } from './components/productlistitem/ProductListItem.component';
+import { ProductListComponent } from './components/productslist/productlist.component';
 
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
@@ -26,19 +28,22 @@ export const sharedConfig: NgModule = {
         LoginComponent,
         NewProductComponent,
         ProductDetailsComponent,
-        ProductListItemComponent
+        ProductListItemComponent,
+        ProductListComponent
     ],
     imports: [
         NguiPopupModule,
         FormsModule,
         CustomFormsModule,
         Ng2UploaderModule,
+        DataTableModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'register', component: RegisterComponent },
             { path: 'login', component: LoginComponent },
             { path: 'newProduct', component: NewProductComponent },
+            { path: 'productList', component: ProductListComponent },
             { path: 'productDetails/:id', component: ProductDetailsComponent },
             { path: '**', redirectTo: 'home' }
         ])
